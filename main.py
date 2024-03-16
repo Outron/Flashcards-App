063708f046c4c8fb4209ea970cb44d6abb85a49e
Outron <hipotyzer@gmail.com>
2024-03-16 22:59:15 +0100

    Added question and answer handling

--- main.py
+++ main.py
@@ -1,11 +1,31 @@
-from flask import Flask, render_template, url_for, redirect
+from flask import Flask, render_template, url_for, redirect, request
+from pymongo import MongoClient
+import random
+
 app = Flask(__name__)
+client = MongoClient('localhost', 27017, username='admin', password='password')
+
+db = client["flashcards_db"]
+flashcards = db["flashcards"]
 
 
-@app.route("/")
+@app.route("/", methods=['GET'])
 def home():
-    return render_template('index.html')
+
+    questions = list(flashcards.find({}, {"_id": 0, "question": 1, "answer": 1}))
+    question = random.choice(questions)
+    return render_template('index.html', question=question['question'], answer=question['answer'])
+
+
+@app.route('/toggle_answer')
+def toggle_answer():
+    # Pobierz parametr z adresu URL
+    show_answer = request.args.get('show_answer')
+
+    # Przekieruj na stronę główną z odpowiednim parametrem w adresie URL
+    return redirect('/?show_answer=true' if show_answer != 'true' else '/')
 
 
 if __name__ == '__main__':
     app.run(debug=True)
+
--- static/script.js
+++ static/script.js
@@ -4,3 +4,29 @@ const body = document.body;
 themeToggle.addEventListener('click', () => {
     body.classList.toggle('dark-theme');
 });
+
+ document.getElementById('flashcard').addEventListener('click', function() {
+     var question = document.getElementById('question');
+     var answer = document.getElementById('answer');
+
+     if (question.classList.contains('visible')) {
+         question.classList.remove('visible');
+         question.classList.add('hidden');
+         answer.classList.remove('hidden');
+         answer.classList.add('visible');
+     }
+     else {
+         answer.classList.remove('visible');
+         answer.classList.add('hidden');
+         question.classList.remove('hidden');
+         question.classList.add('visible');
+    }
+});
+ function toggleCard() {
+            var cardInner = document.querySelector('.card-inner');
+            cardInner.style.transform = cardInner.style.transform === 'rotateY(180deg)' ? 'rotateY(0)' : 'rotateY(180deg)';
+
+            // Pokaż odpowiedź, jeśli była ukryta, lub ukryj, jeśli była widoczna
+            var answer = document.getElementById('answer');
+            answer.classList.toggle('hidden');
+        }
