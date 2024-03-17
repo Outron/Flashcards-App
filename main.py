60df7005e1f33c979fab70d6f321329a7987e540
Outron <hipotyzer@gmail.com>
2024-03-17 17:36:55 +0100

    Added side menu

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for, redirect, request
+from flask import Flask, render_template
 from pymongo import MongoClient
 import random
 
@@ -11,21 +11,10 @@ flashcards = db["flashcards"]
 
 @app.route("/", methods=['GET'])
 def home():
-
     questions = list(flashcards.find({}, {"_id": 0, "question": 1, "answer": 1}))
     question = random.choice(questions)
     return render_template('index.html', question=question['question'], answer=question['answer'])
 
 
-@app.route('/toggle_answer')
-def toggle_answer():
-    # Pobierz parametr z adresu URL
-    show_answer = request.args.get('show_answer')
-
-    # Przekieruj na stronę główną z odpowiednim parametrem w adresie URL
-    return redirect('/?show_answer=true' if show_answer != 'true' else '/')
-
-
 if __name__ == '__main__':
     app.run(debug=True)
-
--- static/script.js
+++ static/script.js
@@ -5,7 +5,7 @@ themeToggle.addEventListener('click', () => {
     body.classList.toggle('dark-theme');
 });
 
- document.getElementById('flashcard').addEventListener('click', function() {
+document.getElementById('flashcard').addEventListener('click', function() {
      var question = document.getElementById('question');
      var answer = document.getElementById('answer');
 
@@ -22,11 +22,17 @@ themeToggle.addEventListener('click', () => {
          question.classList.add('visible');
     }
 });
- function toggleCard() {
-            var cardInner = document.querySelector('.card-inner');
-            cardInner.style.transform = cardInner.style.transform === 'rotateY(180deg)' ? 'rotateY(0)' : 'rotateY(180deg)';
 
-            // Pokaż odpowiedź, jeśli była ukryta, lub ukryj, jeśli była widoczna
-            var answer = document.getElementById('answer');
-            answer.classList.toggle('hidden');
+ function toggleCard() {
+        var cardInner = document.querySelector('.card-inner');
+        cardInner.style.transform = cardInner.style.transform === 'rotateY(180deg)' ? 'rotateY(0)' : 'rotateY(180deg)';
+
+        // Pokaż odpowiedź, jeśli była ukryta, lub ukryj, jeśli była widoczna
+        var answer = document.getElementById('answer');
+        answer.classList.toggle('hidden');
+}
+
+function toggleMenu() {
+            var sideMenu = document.querySelector('.side-menu');
+            sideMenu.style.left = sideMenu.style.left === '0px' ? '-290px' : '0px';
         }
