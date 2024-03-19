1086d6fbfeb8b6bbe32826988c7d1ec217e09383
Outron <hipotyzer@gmail.com>
2024-03-19 02:02:29 +0100

    Added buttons to change flashcards and their handling

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,  request, jsonify
 from pymongo import MongoClient
-import random
 
 app = Flask(__name__)
 client = MongoClient('localhost', 27017, username='admin', password='password')
@@ -9,11 +8,14 @@ db = client["flashcards_db"]
 flashcards = db["flashcards"]
 
 
-@app.route("/", methods=['GET'])
+@app.route("/", methods=['GET', 'POST'])
 def home():
-    questions = list(flashcards.find({}, {"_id": 0, "question": 1, "answer": 1}))
-    question = random.choice(questions)
-    return render_template('index.html', question=question['question'], answer=question['answer'])
+    if request.method == 'GET':
+        questions = list(flashcards.find({}, {"_id": 0, "question": 1, "answer": 1}))
+        return render_template('index.html', questions=questions)
+    elif request.method == 'POST':
+        questions = list(flashcards.find({}, {"_id": 0, "question": 1, "answer": 1}))
+        return jsonify({"questions": questions})
 
 
 if __name__ == '__main__':
--- static/script.js
+++ static/script.js
@@ -5,34 +5,90 @@ themeToggle.addEventListener('click', () => {
     body.classList.toggle('dark-theme');
 });
 
-document.getElementById('flashcard').addEventListener('click', function() {
-     var question = document.getElementById('question');
-     var answer = document.getElementById('answer');
+document.getElementById('flashcard').addEventListener('click', function () {
+    var question = document.getElementById('question');
+    var answer = document.getElementById('answer');
 
-     if (question.classList.contains('visible')) {
-         question.classList.remove('visible');
-         question.classList.add('hidden');
-         answer.classList.remove('hidden');
-         answer.classList.add('visible');
-     }
-     else {
-         answer.classList.remove('visible');
-         answer.classList.add('hidden');
-         question.classList.remove('hidden');
-         question.classList.add('visible');
+    if (question.classList.contains('visible')) {
+        question.classList.remove('visible');
+        question.classList.add('hidden');
+        answer.classList.remove('hidden');
+        answer.classList.add('visible');
+    }
+    else {
+        answer.classList.remove('visible');
+        answer.classList.add('hidden');
+        question.classList.remove('hidden');
+        question.classList.add('visible');
     }
 });
 
- function toggleCard() {
-        var cardInner = document.querySelector('.card-inner');
-        cardInner.style.transform = cardInner.style.transform === 'rotateY(180deg)' ? 'rotateY(0)' : 'rotateY(180deg)';
-
-        // Pokaż odpowiedź, jeśli była ukryta, lub ukryj, jeśli była widoczna
-        var answer = document.getElementById('answer');
-        answer.classList.toggle('hidden');
+function toggleCard(transition) {
+    var cardInner = document.querySelector('.card-inner');
+    cardInner.style.transform = cardInner.style.transform === 'rotateY(180deg)' ? 'rotateY(0)' : 'rotateY(180deg)';
+    if (transition === true) {
+        cardInner.style.transition = 'transform 0.5s';
+    }
+    else {
+        cardInner.style.transition = 'none';
+    }
 }
 
+
+
 function toggleMenu() {
-            var sideMenu = document.querySelector('.side-menu');
-            sideMenu.style.left = sideMenu.style.left === '0px' ? '-290px' : '0px';
+    var sideMenu = document.querySelector('.side-menu');
+    sideMenu.style.left = sideMenu.style.left === '0px' ? '-290px' : '0px';
+}
+
+$(document).ready(function () {
+    var currentIndex = 0;
+    var questions = [];
+
+    $.ajax({
+        url: '/',
+        type: 'POST',
+        success: function (data) {
+            questions = data.questions;
+            showQuestion(currentIndex);
+        },
+        error: function (xhr, status, error) {
+            console.error("Błąd pobierania danych o pytaniach:", error);
         }
+    });
+
+    function showQuestion(index) {
+        var question = questions[index].question;
+        var answer = questions[index].answer;
+        // Pokaż pytanie
+        $('#question').text(question);
+        $('#answer').text(answer);
+
+    }
+
+    $('#button_prev').click(function () {
+        currentIndex = (currentIndex - 1 + questions.length) % questions.length;
+        // if transform is  rotateY(180deg) then toggle
+        if (document.querySelector('.card-inner').style.transform === 'rotateY(180deg)') {
+
+            toggleCard(false);
+            showQuestion(currentIndex);
+        }
+        else {
+            showQuestion(currentIndex);
+        }
+
+    });
+
+    $('#button_next').click(function () {
+        currentIndex = (currentIndex + 1) % questions.length;
+        if (document.querySelector('.card-inner').style.transform === 'rotateY(180deg)') {
+            toggleCard(false);
+            showQuestion(currentIndex);
+        }
+        else {
+            showQuestion(currentIndex);
+        }
+
+    });
+});
