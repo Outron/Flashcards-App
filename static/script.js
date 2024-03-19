5521165e20ee2ae5307d61b59466722ab548d52a
Outron <hipotyzer@gmail.com>
2024-03-19 06:08:20 +0100

    small design changes

--- static/script.js
+++ static/script.js
@@ -5,24 +5,6 @@ themeToggle.addEventListener('click', () => {
     body.classList.toggle('dark-theme');
 });
 
-document.getElementById('flashcard').addEventListener('click', function () {
-    var question = document.getElementById('question');
-    var answer = document.getElementById('answer');
-
-    if (question.classList.contains('visible')) {
-        question.classList.remove('visible');
-        question.classList.add('hidden');
-        answer.classList.remove('hidden');
-        answer.classList.add('visible');
-    }
-    else {
-        answer.classList.remove('visible');
-        answer.classList.add('hidden');
-        question.classList.remove('hidden');
-        question.classList.add('visible');
-    }
-});
-
 function toggleCard(transition) {
     var cardInner = document.querySelector('.card-inner');
     cardInner.style.transform = cardInner.style.transform === 'rotateY(180deg)' ? 'rotateY(0)' : 'rotateY(180deg)';
