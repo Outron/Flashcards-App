94c86a66c2d865aef77412fee5a382b8dd9f55ad
Dawid <49493191+Outron@users.noreply.github.com>
2025-11-02 21:27:12 +0100

    update var

--- Terraform/modules/artifact_registry/outputs.tf
+++ Terraform/modules/artifact_registry/outputs.tf
@@ -1,9 +1,9 @@
 output "repository_id" {
   description = "ID of created repository."
-  value       = google_artifact_registry_repository.repo.repository_id
+  value       = google_artifact_registry_repository.repo_id.repository_id
 }
 
 output "path_prefix" {
   description = "Prefix to repository in AR format"
-  value       = "${var.region}-docker.pkg.dev/${var.project_id}/${google_artifact_registry_repository.repo.repository_id}"
+  value       = "${var.region}-docker.pkg.dev/${var.project_id}/${google_artifact_registry_repository.repo_id.repository_id}"
 }
